22f4bc0991bfa71e1c6d4ab0aefe6b04e0aaf1c7
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-08-20 21:57:56 -0400

    add 1st test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,31 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
 	// Arrange
+	arg := CreateAccountParams{
+		Owner:    "tom",
+		Balance:  100,
+		Currency: "USD",
+	}
+
+	// Act
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+
+	// Expect
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
 }
