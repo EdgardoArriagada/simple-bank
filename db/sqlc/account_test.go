e2e05c789e260d0f110804b018e66f7baed3d0c2
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-08-25 12:14:01 -0400

    expand coverage

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	// Arrange
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
@@ -29,4 +29,23 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account1, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account1)
+
+	require.Equal(t, account1.ID, account1.ID)
+	require.Equal(t, account1.Owner, account1.Owner)
+	require.Equal(t, account1.Balance, account1.Balance)
+	require.Equal(t, account1.Currency, account1.Currency)
 }
